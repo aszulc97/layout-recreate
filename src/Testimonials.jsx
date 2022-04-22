cbb5ae6c42e58637296834a8ce5313c11e9ea3aa
Agata Szulc <agataszulc97@gmail.com>
2022-04-22 10:22:21 +0200

    icons as props

--- src/Testimonials.jsx
+++ src/Testimonials.jsx
@@ -9,23 +9,74 @@ export default function Testimonials() {
         <p>Stacy's mom has got it goin' on she's all I want and I've waited so long</p>
       </div>
       <div id="testimonialCont">
-        <Testimonial name="Agatka" title="Some chicken" />
-        <Testimonial name="Zuzka" title="The dog" />
-        <Testimonial name="Yo mama" title="Yo mama" />
-        <Testimonial name="Jeff Bezos" title="CEO" />
+        {TestimonialsArray.map((testimonial) => (
+          <Testimonial
+            text={testimonial.text}
+            name={testimonial.author}
+            title={testimonial.title}
+            companyname={testimonial.companyname}
+            icon={testimonial.icon}
+          />
+        ))}
       </div>
     </section>
   );
 }
 
-function Testimonial({ name, title }) {
+const TestimonialsArray = [
+  {
+    text: "When potential customers are researching you online, they're getting to know you through the content on your website. So understandably, many of them might be skeptical or hesitant to trust you right away.",
+    author: "Agatka",
+    title: "Some chicken",
+    companyname: "Agata & Co.",
+    icon: (
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-flag" viewBox="0 0 16 16">
+        <path d="M14.778.085A.5.5 0 0 1 15 .5V8a.5.5 0 0 1-.314.464L14.5 8l.186.464-.003.001-.006.003-.023.009a12.435 12.435 0 0 1-.397.15c-.264.095-.631.223-1.047.35-.816.252-1.879.523-2.71.523-.847 0-1.548-.28-2.158-.525l-.028-.01C7.68 8.71 7.14 8.5 6.5 8.5c-.7 0-1.638.23-2.437.477A19.626 19.626 0 0 0 3 9.342V15.5a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 1 0v.282c.226-.079.496-.17.79-.26C4.606.272 5.67 0 6.5 0c.84 0 1.524.277 2.121.519l.043.018C9.286.788 9.828 1 10.5 1c.7 0 1.638-.23 2.437-.477a19.587 19.587 0 0 0 1.349-.476l.019-.007.004-.002h.001M14 1.221c-.22.078-.48.167-.766.255-.81.252-1.872.523-2.734.523-.886 0-1.592-.286-2.203-.534l-.008-.003C7.662 1.21 7.139 1 6.5 1c-.669 0-1.606.229-2.415.478A21.294 21.294 0 0 0 3 1.845v6.433c.22-.078.48-.167.766-.255C4.576 7.77 5.638 7.5 6.5 7.5c.847 0 1.548.28 2.158.525l.028.01C9.32 8.29 9.86 8.5 10.5 8.5c.668 0 1.606-.229 2.415-.478A21.317 21.317 0 0 0 14 7.655V1.222z" />
+      </svg>
+    ),
+  },
+  {
+    text: "To prove the value of what you have to offer, why not let your happy customers do the talking? To generate these glowing testimonials, you need to know who your happy, successful customers are — and that's where collecting and analyzing customer feedback comes in.",
+    author: "Zuzka",
+    title: "The dog",
+    companyname: "Dogstudio",
+    icon: (
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-award" viewBox="0 0 16 16">
+        <path d="M9.669.864L8 0 6.331.864l-1.858.282-.842 1.68-1.337 1.32L2.6 6l-.306 1.854 1.337 1.32.842 1.68 1.858.282L8 12l1.669-.864 1.858-.282.842-1.68 1.337-1.32L13.4 6l.306-1.854-1.337-1.32-.842-1.68L9.669.864zm1.196 1.193l.684 1.365 1.086 1.072L12.387 6l.248 1.506-1.086 1.072-.684 1.365-1.51.229L8 10.874l-1.355-.702-1.51-.229-.684-1.365-1.086-1.072L3.614 6l-.25-1.506 1.087-1.072.684-1.365 1.51-.229L8 1.126l1.356.702 1.509.229z" />
+        <path d="M4 11.794V16l4-1 4 1v-4.206l-2.018.306L8 13.126 6.018 12.1 4 11.794z" />
+      </svg>
+    ),
+  },
+  {
+    text: "But effective testimonials go beyond a simple quote that proclaims your greatness. They need to resonate with your target audience and the people who could also potentially benefit from the work you do in the future. That's why great testimonials also tell a story — one that inspires and motivates the people reading it.",
+    author: "Yo mama",
+    title: "Yo mama",
+    companyname: "Yo mama is big",
+    icon: (
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-grid" viewBox="0 0 16 16">
+        <path d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5v-3zM2.5 2a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zm6.5.5A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zM1 10.5A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zm6.5.5A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3z" />
+      </svg>
+    ),
+  },
+  {
+    text: "Your testimonial page serves as a platform to show off how others have benefited from your product or service, making it a powerful tool for establishing trust and encouraging potential buyers to take action. Plus, having a testimonial page serves as yet another indexed page on your website. It contains content covering product features, pain points, and keywords your marketing team is trying to rank for in search.",
+    author: "Jeff Bezos",
+    title: "CEO",
+    companyname: "Amazon",
+    icon: (
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-kanban" viewBox="0 0 16 16">
+        <path d="M13.5 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1h-11a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h11zm-11-1a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h11a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2h-11z" />
+        <path d="M6.5 3a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1V3zm-4 0a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1V3zm8 0a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1V3z" />
+      </svg>
+    ),
+  },
+];
+
+function Testimonial({ text, name, title, companyname, icon }) {
   return (
     <div className="testimonial">
-      <LogoItem />
-      <p>
-        I am sitting in the morning at the diner on the corner I am waiting at the counter for the man to pour the coffee And he
-        fills it only halfway and before I even argue He is looking out the window at somebody coming in
-      </p>
+      <LogoItem companyname={companyname} icon={icon} />
+      <p>{text}</p>
       <Author name={name} title={title} />
     </div>
   );
